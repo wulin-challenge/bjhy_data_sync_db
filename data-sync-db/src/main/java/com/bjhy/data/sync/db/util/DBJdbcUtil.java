c9e066f58abc8092dc43c618791dc1f397e0a888
wulin-challenge <1178649872@qq.com>
2019-03-27 18:24:34 +0800

    修复连接打印不出元数据的问题

--- data-sync-db/src/main/java/com/bjhy/data/sync/db/util/DBJdbcUtil.java
+++ data-sync-db/src/main/java/com/bjhy/data/sync/db/util/DBJdbcUtil.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
@@ -44,15 +46,112 @@ public class DBJdbcUtil {
         try {
             x.close();
         } catch (Exception e) {
-        	String userName = null;
-        	String url = null;
-        	try {
-        		userName = x.getMetaData().getUserName();
-        		url = x.getMetaData().getURL();
-			} catch (SQLException e1) {
-				logger.error("得到元数据信息错误"+e1.getMessage());
-			}
-        	logger.error("userName:"+userName+",url:"+url+",close connection error : "+e.getMessage());
+        	ConnectionMetaData connectionMetaData = getConnectionMetaData(x);
+        	String userName = connectionMetaData.userName();
+        	String database = connectionMetaData.database();
+        	logger.error("关闭连接错误 : userName:"+userName+",database:"+database+",详细信息"+e.getMessage());
         }
+    }
+    
+    /**
+     * 得到连接元数据
+     * @return
+     */
+    private static ConnectionMetaData getConnectionMetaData(Connection x){
+    	
+    	if(x == null) return new DefaultConnectionMetaData();
+    	
+    	Set<ConnectionMetaData> cmdRegisterList = new HashSet<ConnectionMetaData>();
+    	cmdRegisterList.add(new OracleConnectionMetaData(x));
+    	
+    	for (ConnectionMetaData connectionMetaData : cmdRegisterList) {
+			if(x != null && x.getClass().getName().equals(connectionMetaData.implClassName())){
+				return connectionMetaData;
+			}
+		}
+    	return new DefaultConnectionMetaData();
+    }
+    
+    /**
+     * 连接元数据
+     * @author wubo
+     *
+     */
+    private static interface ConnectionMetaData{
+    	
+    	/**
+    	 * 用户名
+    	 * @return
+    	 */
+    	String userName();
+    	
+    	/**
+    	 * 数据库
+    	 * @return
+    	 */
+    	String database();
+    	
+    	/**
+    	 * 实现类名称
+    	 * @return
+    	 */
+    	String implClassName();
+    }
+    
+    /**
+     * Oracle的连接元数据实现类
+     * @author wubo
+     */
+    private static class OracleConnectionMetaData implements ConnectionMetaData{
+    	private String userName;
+    	private String database;
+    	private Connection x;
+    	
+		public OracleConnectionMetaData(Connection x) {
+			this.x = x;
+		}
+
+		@Override
+		public String userName() {
+			if(this.userName == null){
+				userName = (String) ReflectUtil.getFieldValue(x,x.getClass().getSuperclass(), "userName");
+			}
+			return userName;
+		}
+
+		@Override
+		public String database() {
+			if(this.database == null){
+				database = (String) ReflectUtil.getFieldValue(x,x.getClass().getSuperclass(), "database");
+			}
+			return database;
+		}
+
+		@Override
+		public String implClassName() {
+			return "oracle.jdbc.driver.T4CConnection";
+		}
+    }
+    
+    /**
+     * 默认的连接元数据实现类
+     * @author wubo
+     */
+    private static class DefaultConnectionMetaData implements ConnectionMetaData{
+
+		@Override
+		public String userName() {
+			return "";
+		}
+
+		@Override
+		public String database() {
+			return "";
+		}
+
+		@Override
+		public String implClassName() {
+			return "";
+		}
     }
 }
